5a4f8ab4235137b78aaa877624837f8527871b3b
Lukas Schwab <lukas.schwab@gmail.com>
2018-09-20 10:33:03 -0700

    Update styles for new Trello styles

--- trell/styles.js
+++ trell/styles.js
@@ -1,1 +1,2 @@
-document.getElementsByClassName('mod-add')[0].setAttribute("style", "width: 100px;")
+document.getElementsByClassName('mod-add')[0].setAttribute("style", "width: 41px;")
+document.getElementsByClassName('mod-add')[0].children[0].children[0].children[0].style.visibility = "collapse"
